f4648e267bf3c61e0586e9be2d31101f26e1f9de
web.ljg <web.ljg@gmail.com>
2022-08-11 18:01:16 +0900

    styled-components 사용 예시

--- class-one/src/App.js
+++ class-one/src/App.js
@@ -1,23 +1,60 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Container = styled.section`
+const Wrapper = styled.div`
   display: flex;
-`
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  overflow:hidden;
+`;
+
+const rotateAni = keyframes`
+  0% {
+    border-radius: 0;
+    transform: rotate(0deg);
+  } 
+  50% {
+    border-radius: 100px;
+    transform: rotate(360deg);
+  }
+  100% {
+    border-radius: 0;
+    transform: rotate(0deg);
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
 
 const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: ${props => props.bgColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotateAni} 2s linear infinite;
+  ${Emoji}{
+    &:hover{
+      transform: scale(1.5);
+      transition: 0.3s ease;
+    }
+    &:active{
+      opacity: 0;
+    }
+  }
 `
+
 
 const App = () => {
   return (
-    <>
-      <Container>
-        <Box bgColor='teal' />
-        <Box bgColor='tomato' />
-      </Container>
-    </>
+    <Wrapper>
+      <Box>
+        <Emoji as="p">😀</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
