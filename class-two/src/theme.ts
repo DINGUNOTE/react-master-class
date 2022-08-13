7471ae8aeda393f043a6e4eb093472d4664fcd32
web.ljg <web.ljg@gmail.com>
2022-08-14 08:36:46 +0900

    theme component init

--- class-two/src/theme.ts
+++ class-two/src/theme.ts
@@ -1,13 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
-export const lightTheme: DefaultTheme = {
+export const theme: DefaultTheme = {
   textColor: '#222',
   bgColor: '#fff',
   btnColor: 'tomato',
-};
-
-export const darkTheme: DefaultTheme = {
-  textColor: '#fff',
-  bgColor: '#222',
-  btnColor: 'teal',
 };
