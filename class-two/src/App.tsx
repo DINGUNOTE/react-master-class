ca3cccd3ad35d2113bbda3a2c2debeb7858ce013
web.ljg <web.ljg@gmail.com>
2022-08-14 08:37:07 +0900

    feat: Component Router connect

--- class-two/src/App.tsx
+++ class-two/src/App.tsx
@@ -1,19 +1,7 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from './Router';
 
 const App = () => {
-  return (
-    <Container>
-      <Title>protected</Title>
-    </Container>
-  );
+  return <Router />;
 };
 
 export default App;
