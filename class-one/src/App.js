a0fce5e8bac156e70e3331ee705f2e2b7be17cdd
web.ljg <web.ljg@gmail.com>
2022-09-03 13:20:38 +0900

    feat: layout animation test

--- class-one/src/App.js
+++ class-one/src/App.js
@@ -10,82 +10,69 @@ import { useEffect, useState } from 'react';
 
 const Wrapper = styled(motion.div)`
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 10vw;
   width: 100vw;
   height: 100vh;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
   overflow: hidden;
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
+  width: 50vw;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  position: absolute;
-  top: 50px;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  entry: (isPrev) => ({
-    x: isPrev ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 0.3 },
-  },
-  exit: (isPrev) => ({
-    x: isPrev ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: { duration: 0.3 },
-  }),
-};
+const Overlay = styled(motion.div)`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
 
 const App = () => {
-  const [isPrev, setIsPrev] = useState(false);
-
-  const [visible, setVisible] = useState(1);
-  const prevSlide = () => {
-    setIsPrev(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
-  const nextSlide = () => {
-    setIsPrev(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
+  const Arr = [1, 2, 3, 4];
+  const [id, setId] = useState(null);
+  console.log(id);
 
   return (
     <Wrapper>
-      <AnimatePresence custom={isPrev} mode="wait">
-        <Box
-          key={visible}
-          variants={boxVariants}
-          custom={isPrev}
-          initial="entry"
-          animate="center"
-          exit="exit"
-        >
-          {visible}
-        </Box>
+      <Grid>
+        {Arr.map((n) => (
+          <Box key={n} layoutId={n} onClick={() => setId(n)} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+            animate={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+            exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+            onClick={() => setId(null)}
+          >
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-
-      <button type="button" onClick={prevSlide}>
-        prev
-      </button>
-      <button type="button" onClick={nextSlide}>
-        next
-      </button>
     </Wrapper>
   );
 };
